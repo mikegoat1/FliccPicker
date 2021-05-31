43fdf52348e7f3020ffca0514f6a88952983beef
Alys Scorpio <alyssad1182@gmail.com>
2021-05-31 16:29:08 -0700

    creating variables for HTML elements

--- assets/js/starterPage.js
+++ assets/js/starterPage.js
@@ -13,6 +13,38 @@ var urlSourceNetflixTitles = "https://cors-anywhere.herokuapp.com/https://api.wa
 var streamArray = ["Netflix", "Hulu", "Amazon", "HBO", "Disney+", "AppleTV"]
 var genreArray = ["Action & Adventure", "Animation", "Anime", "Comedy", "Crime", "Documentary", "Drama", "Family", "Horror", "Kids", "Musical", "Mystery", "Romance", "Sci-Fi & Fantasy", "Sports", "Supernatural", "Thriller", "War", "Western"]
 
+// Variables for HTML streaming service checkboxes
+var netflix = document.querySelector('#check-netflix');
+var hulu = document.querySelector('#check-hulu');
+var prime = document.querySelector('#check-prime');
+var hbo = document.querySelector('#check-hbo');
+var disney = document.querySelector('#check-disney');
+var apple = document.querySelector('#check-apple');
+
+// Variables for HTML genre checkboxes
+var action = document.querySelector('#check-action');
+var anime = document.querySelector('#check-anime');
+var animation = document.querySelector('#check-animation');
+var comedy = document.querySelector('#check-comedy');
+var crime = document.querySelector('#check-crime');
+var drama = document.querySelector('#check-drama');
+var documentary = document.querySelector('#check-documentary');
+var family = document.querySelector('#check-family');
+var horror = document.querySelector('#check-horror');
+var kids = document.querySelector('#check-kids');
+var musical = document.querySelector('#check-musical');
+var mystery = document.querySelector('#check-mystery');
+var romance = document.querySelector('#check-romance');
+var sciFi = document.querySelector('#check-scifi');
+var sports = document.querySelector('#check-sports');
+var supernatural = document.querySelector('#check-supernatural');
+var thriller = document.querySelector('#check-thriller');
+var war = document.querySelector('#check-war');
+var western = document.querySelector('#check-western');
+
+// Variable for HTML submit button
+var submitBtn = document.querySelector('#submit');
+
 fetch(urlGenres)
     .then(function (response) {
         return response.json();
