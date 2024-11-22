867a8a3d8ff1bd5c8bbad6c866a371e2baf07aa3
mikegoat1 <mikeg.o.a.t.1@gmail.com>
2024-11-22 09:38:55 -0800

    Refactor

--- assets/js/starterPage.js
+++ assets/js/starterPage.js
@@ -14,14 +14,15 @@ $(document).ready(function () {
     });
 
     // Grabs Selected from Starter Page
+    console.log(window.location.origin )
     function sourceCheck() {
         let genreInputVal = $("#genre-input").val();
         // console.log(genreInputVal);
 
-        let queryString = window.location.origin + "FliccPicker/views/results_page.html?genrelist=" + encodeURIComponent(genreInputVal);
+        let queryString = window.location.origin + "/views/results_page.html?genrelist=" + encodeURIComponent(genreInputVal);
+        console.log(queryString);
         document.location.replace(queryString);
     }
-
     // On click of submit button calls function sourceCheck
     $("button[value=button]").on("click", function (event) {
         event.preventDefault();
